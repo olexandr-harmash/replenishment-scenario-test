dd63f0dcf1fa9daf4f81902392bd728f5d023b43
o.harmash <o.harmash@gamzix.com>
2024-10-08 12:44:35 +0300

    RT-3, add identity server.

---
+++ src/Identity/Program.cs
@@ -0,0 +1,37 @@
+﻿using Identity;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+    
+    app.Run();
+}
+catch (HostAbortedException)
+{
+    Log.Information("Shut down triggered by entity framework tools");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
--- src/web/src/index.tsx
+++ src/web/src/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { AuthProvider } from "react-oidc-context";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { User, WebStorageStateStore } from 'oidc-client-ts';
+
+const oidcConfig = {
+  authority: "https://localhost:5001/",
+  client_id: "web",
+  client_secret: "secret",
+  redirect_uri: "http://localhost:3000/",
+  post_logout_redirect_uri: "http://localhost:3000/",
+  response_type: "code",  // или "id_token token"
+  scope: "openid profile api1",
+  // post_logout_redirect_uri: "https://your-app.com/logout",
+  // silent_redirect_uri: "https://your-app.com/silent-renew",
+  // automaticSilentRenew: true,
+  //loadUserInfo: true,
+  onSigninCallback: (user: User | void): void => {
+    window.history.replaceState(
+        {},
+        document.title,
+        window.location.pathname
+    );
+  },
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <AuthProvider {...oidcConfig}>
     <App />
-  </React.StrictMode>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
